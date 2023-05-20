617eda969a5b8cffb8302d5f930f80c9a1d16e47
arkady <archpod@gmail.com>
2023-05-20 16:14:27 +0300

    Added MoviesView and MovieDetailView to movies/views.py

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Movie
 
-# Create your views here.
+
+class MoviesView(ListView):
+    """Список фильмов"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+
+
+class MovieDetailView(DetailView):
+    """Полное описание фильма"""
+    model = Movie
+    slug_field = "url"
