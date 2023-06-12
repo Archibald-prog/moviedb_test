134c9f20c4e70c6920449e2068eb2e779709f318
arkady <archpod@gmail.com>
2023-06-12 10:51:28 +0300

    Added CategoryAdmin class to movies/admin.py

--- movies/admin.py
+++ movies/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Категории"""
+    list_display = ("id", "name", "url")
+    list_display_links = ("name",)
+
+
 admin.site.register(Genre)
 admin.site.register(Movie)
 admin.site.register(MovieShots)
